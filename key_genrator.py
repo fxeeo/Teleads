5acae3748e37c8d27ffd79f11e06780ced4f8d00
fxeeo <q8rktt@gmail.com>
2025-05-25 03:30:49 +0530

    Update key_genrator.py

--- key_genrator.py
+++ key_genrator.py
@@ -216,4 +216,5 @@ def check_key_api():
 
 if __name__ == '__main__':
     init_database()
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
